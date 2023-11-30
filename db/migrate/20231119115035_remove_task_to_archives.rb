814915f8769749a9f7b649a7af45239d4345cf5b
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2023-11-30 15:51:55 +0900

    Delete 20231119115035_remove_task_to_archives.rb

--- db/migrate/20231119115035_remove_task_to_archives.rb
+++
@@ -1,6 +0,0 @@
-class RemoveTaskToArchives < ActiveRecord::Migration[7.0]
-  def change
-    # add_column :archives, :task_id, :bigint, null: false
-    add_foreign_key :archives, :tasks, column: :task_id
-  end
-end
