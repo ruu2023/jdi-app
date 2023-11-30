d7c4cbb62db5f48225230e2f50ca4a91c90aa40c
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2023-11-30 15:38:36 +0900

    Update 20231114151239_add_row_order_to_tasks.rb

--- db/migrate/20231114151239_add_row_order_to_tasks.rb
+++ db/migrate/20231114151239_add_row_order_to_tasks.rb
@@ -1,6 +1,6 @@
 class AddRowOrderToTasks < ActiveRecord::Migration[7.0]
   def change
     # add_column :tasks, :row_order, :integer
-    remove_column :position
+    remove_column :tasks, :position
   end
 end
