7a17af4e6c8809b7a0370c79fde2eb7229a32821
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2024-01-13 15:08:24 +0900

    password編集機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def edit
-
+    
   end
 
   def update
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
