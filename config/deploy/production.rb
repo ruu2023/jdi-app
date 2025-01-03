0cc1ee102e1f50144b60c291960b34358fb2aa11
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2025-01-03 09:37:28 +0900

    mod

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,4 +4,4 @@
 server '160.251.234.68', user: 'root', roles: %w{app db web}, port: 50022
 
 #デプロイするサーバーにsshログインする鍵の情報。サーバー編で作成した鍵のパス
-# set :ssh_options, keys: '~/.ssh/conoha_hoge/id_rsa'
+set :ssh_options, keys: '~/downloads/key-2024-12-27-14-58.pem'
